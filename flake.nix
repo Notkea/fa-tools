cb893cda0fddf2e59e5e6ffa93b88639a8e3f182
Notkea <notkea@pacien.net>
2022-11-06 17:09:01 +0100

    flake: update nixpkgs 21.05 -> 22.05

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
 
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
