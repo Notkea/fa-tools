f3be457c3d74922222058f32e83dcefe935f4a2d
Notkea <notkea@pacien.net>
2023-05-04 02:27:53 +0200

    download-ebook: use common metadata extraction

--- download-ebook.py
+++ download-ebook.py
@@ -5,41 +5,9 @@
 # Licensed under the EUPL version 1.2
 
 from _utils import *
+from _submission import SubmissionMetadata, extract_submission_metadata
 from sys import argv
-from dataclasses import dataclass
 from markdownify import markdownify
-
-@dataclass(frozen=True)
-class SubmissionMetadata:
-  url: str
-  download_url: str
-  title: str
-  author: str
-  date: str
-  tags: list[str]
-  folder_names: list[str]
-
-def extract_folders_links(page):
-  folders_container = page.find(class_='folder-list-container')
-  if folders_container is None: return []
-  return folders_container.find_all('span')
-
-def extract_tags(page):
-  tags_container = page.find(class_='tags-row')
-  if tags_container is None: return []
-  return [ a.text for a in tags_container.find_all('a') ]
-
-def extract_submission_metadata(page, url):
-  meta_container = page.find(class_='submission-id-sub-container')
-  return SubmissionMetadata(
-    url=url,
-    download_url=qualify(page.find(class_='download').find('a')['href']),
-    title=meta_container.find(class_='submission-title').find('p').text,
-    author=meta_container.find('a').find('strong').text,
-    date=meta_container.find(class_='popup_date')['title'],
-    tags=extract_tags(page),
-    folder_names=[ s.text for s in extract_folders_links(page) ],
-  )
 
 def format_list(lst):
   return ', '.join(lst) if lst else '_none_'
@@ -58,18 +26,6 @@ def format_pandoc_markdown_headers(sub_meta):
     '',
   ])
 
-def extract_raw_description(page):
-  return str(page.find(class_='submission-description'))
-
-def extract_raw_inline_writing(page):
-  # Use FA's rendered BBCode and convert it back to MD
-  writing_container = page.find(class_='submission-writing')
-  if writing_container is None: return None
-  preview = writing_container.find('center').find('div')
-  paragraphs = preview.contents[8:]  # skip file info
-  if len(paragraphs) <= 1: return None  # no preview, unsupported file type
-  return ''.join(str(p) for p in paragraphs)
-
 def sanitise_text(raw):
   return raw.strip().replace('\xa0', ' ')  # NBSP
 
@@ -82,7 +38,7 @@ def submission_to_markdown(url):
   metadata = extract_submission_metadata(page, url)
   print(format_pandoc_markdown_headers(metadata))
 
-  content = extract_raw_inline_writing(page) or extract_raw_description(page)
+  content = metadata.inline_writing_html or metadata.description
   print(to_markdown(content))
 
 if __name__ == "__main__":
