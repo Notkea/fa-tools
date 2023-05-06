f210f44077d9995bf3f9cb52cd10b8a2aa092e5c
Notkea <notkea@pacien.net>
2023-05-06 23:59:52 +0200

    download: add file download command with session

--- app/Main.hs
+++ app/Main.hs
@@ -2,22 +2,78 @@
 -- Copyright 2023 Notkea
 -- Licensed under the EUPL version 1.2
 
+import qualified Network.HTTP.Client as HTTP
+import qualified Network.HTTP.Conduit as HTTPC
+import qualified Data.Text as T
+import qualified Conduit as C
+
+import Conduit ((.|))
+import Data.ByteString (ByteString)
+import System.Environment (getEnv)
+import System.FilePath.Posix (takeBaseName)
+import Control.Monad.Trans.Resource (runResourceT)
+
 import System.Console.CmdArgs
+import Fa.Client
+
+envKeyFaSessionHeaders :: String
+envKeyFaSessionHeaders = "FA_SESSION_HEADERS"
+
+envHelp :: [String]
+envHelp = map (\(key, text) -> "$" ++ key ++ ": " ++ text)
+  [ (envKeyFaSessionHeaders, "headers of an existing session")
+  ]
+
+initHttpManager :: IO HTTP.Manager
+initHttpManager = do
+  headerLines <- getEnv envKeyFaSessionHeaders
+  let headers = parseHeaderLines $ T.pack headerLines
+  newHttpManagerWithSession $ filterSessionHeaders headers
+
+type URL = String
 
 data Options
-  = Dummy
+  = Download
+      { url :: URL
+      , output :: Maybe FilePath
+      }
+  | Dummy
   deriving (Show, Data, Typeable)
 
 optionsModes :: Options
 optionsModes = modes
-  [ Dummy
+  [ Download
+      { url = def
+          &= typ "FILE_URL"
+          &= argPos 0
+      , output = Nothing
+          &= typFile
+          &= help "Output file (default: name from URL)"
+      }
+  , Dummy
   ]
   &= summary "A CLI toolbox to download content from FurAffinity."
   &= program "fa-tools"
+  &= help (unlines envHelp)
   &= helpArg [explicit, name "h", name "help"]
   &= versionArg [explicit, name "version"]
 
 main :: IO ()
 main = do
   arguments <- cmdArgs optionsModes
-  print arguments
+  client <- initHttpManager
+  run client arguments
+
+run :: HTTP.Manager -> Options -> IO ()
+run client Download { url, output } = do
+  request <- HTTP.parseRequest url
+  runResourceT $ do
+    response <- HTTPC.http request client
+    C.runConduit $ HTTPC.responseBody response .| sink output
+  where
+    sink :: C.MonadResource m => Maybe FilePath -> C.ConduitT ByteString o m ()
+    sink (Just "-") = C.stdoutC
+    sink (Just path) = C.sinkFile path
+    sink Nothing = C.sinkFile $ takeBaseName url
+
+run _ Dummy = return ()
---
+++ lib/Fa/Client.hs
@@ -0,0 +1,45 @@
+-- File part of fa-tools
+-- Copyright 2023 Notkea
+-- Licensed under the EUPL version 1.2
+
+module Fa.Client where
+
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import qualified Data.CaseInsensitive as CI
+import qualified Network.HTTP.Client as HTTP
+import qualified Network.HTTP.Client.TLS as HTTP
+import qualified Network.HTTP.Types.Header as HTTP
+
+import Control.Arrow ((>>>))
+import Data.Maybe (mapMaybe)
+
+type RequestModifier = HTTP.Request -> IO HTTP.Request
+
+addingHeaders :: HTTP.RequestHeaders -> RequestModifier
+addingHeaders headers req = do
+  req' <- HTTP.managerModifyRequest HTTP.tlsManagerSettings req
+  return $ req' {
+    HTTP.requestHeaders = headers ++ HTTP.requestHeaders req'
+  }
+
+newHttpManagerWithSession :: HTTP.RequestHeaders -> IO HTTP.Manager
+newHttpManagerWithSession headers = HTTP.newManager $ HTTP.tlsManagerSettings {
+  HTTP.managerModifyRequest = addingHeaders headers
+}
+
+filterSessionHeaders :: HTTP.RequestHeaders -> HTTP.RequestHeaders
+filterSessionHeaders = filter (flip notElem excludedHeaders . fst)
+  where
+    excludedHeaders :: [HTTP.HeaderName]
+    excludedHeaders = map (CI.mk . T.encodeUtf8)
+      [ "host"
+      , "connect"
+      ]
+
+parseHeaderLines :: T.Text -> HTTP.RequestHeaders
+parseHeaderLines = T.lines >>> map (T.splitOn ": ") >>> mapMaybe toHeader
+  where
+    toHeader :: [T.Text] -> Maybe HTTP.Header
+    toHeader [key, val] = Just (CI.mk $ T.encodeUtf8 key, T.encodeUtf8 val)
+    toHeader _ = Nothing
