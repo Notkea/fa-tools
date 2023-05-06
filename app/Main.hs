980db97c4a498f387b52fd8f4b5c113395f6ea0b
Notkea <notkea@pacien.net>
2023-05-06 15:14:25 +0200

    app: configure cmdargs

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,22 @@
 -- Copyright 2023 Notkea
 -- Licensed under the EUPL version 1.2
 
+import System.Console.CmdArgs
+
+data Options
+  = Dummy
+  deriving (Show, Data, Typeable)
+
+optionsModes :: Options
+optionsModes = modes
+  [ Dummy
+  ]
+  &= summary "A CLI toolbox to download content from FurAffinity."
+  &= program "fa-tools"
+  &= helpArg [explicit, name "h", name "help"]
+  &= versionArg [explicit, name "version"]
+
 main :: IO ()
-main = putStrLn "hello world"
+main = do
+  arguments <- cmdArgs optionsModes
+  print arguments
